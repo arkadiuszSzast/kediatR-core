8af5413ee99824b667ed1e32cec02cadcf4b1686
arkadiusz <szastarek@gmail.com>
2022-03-30 17:34:01 +0200

    add command metadata

--- src/main/kotlin/com/trendyol/kediatr/CommandMetadata.kt
+++ src/main/kotlin/com/trendyol/kediatr/CommandMetadata.kt
@@ -2,4 +2,4 @@ package com.trendyol.kediatr
 
 import java.util.UUID
 
-data class CommandMetadata(val correlationId: UUID, val causationId: UUID)
+data class CommandMetadata(val correlationId: UUID?, val causationId: UUID?)
